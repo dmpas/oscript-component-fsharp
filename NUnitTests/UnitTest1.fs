6aaa7741dbbc596aeb1f6234326ed97e2d79bb4d
Sergey Batanov <sergey.batanov@dmpas.ru>
2025-08-15 21:57:18 +0300

    NUnit

--- NUnitTests/UnitTest1.fs
+++ NUnitTests/UnitTest1.fs
@@ -1,6 +1,12 @@
 module NUnitTests
 
+open OneScript.StandardLibrary.Collections
+open ScriptEngine.HostedScript.Extensions
+open ScriptEngine.Hosting
+open OneScript.Types
+
 open NUnit.Framework
+open OsComponentFSharp
 
 [<SetUp>]
 let Setup () =
@@ -8,4 +14,14 @@ let Setup () =
 
 [<Test>]
 let Test1 () =
-    Assert.Pass()
+    
+    let Engine = DefaultEngineBuilder.Create().SetupConfiguration( ignore ).SetDefaultOptions().UseImports().UseNativeRuntime().UseFileSystemLibraries().Build()
+    
+    Engine.AttachAssembly(System.Reflection.Assembly.GetAssembly(typeof<ArrayImpl>))
+    Engine.AttachAssembly(System.Reflection.Assembly.GetAssembly(typeof<MyClass>))
+
+    let context = TypeActivationContext(CurrentProcess = Engine.NewProcess())
+    let instance = MyClass(context, "123")
+    Assert.AreEqual(instance.MyReadOnlyProperty, "123")
+    let myMethodResult = instance.MyMethod 1 2
+    Assert.AreEqual(myMethodResult, 3)
